7b2139bb7baf04d9b0bcf279877dd20da187e821
Nova <noah.trauben@gmail.com>
2018-11-11 04:07:34 -0500

    .

--- src/findPort.py
+++ src/findPort.py
@@ -2,12 +2,15 @@ import subprocess
 
 def findParticle():
     if str(subprocess.call("udevadm info /dev/ttyACM0 | head -3 | tail -1", shell=True)) == "S: serial/by-id/usb-Particle_Photon_2c0027001051363036373538-if00":
+        print("Particle @/dev/ttyACM0")
         return "/dev/ttyACM0"
     else:
+        print("Particle @/dev/ttyACM1")
         return "/dev/ttyACM1"
 def findScreen():
     if str(subprocess.call("udevadm info /dev/ttyACM0 | head -3 | tail -1", shell=True)) == "S: serial/by-id/usb-239a_Adafruit_Industries-if00":
+        print("Screen @/dev/ttyACM0")
         return "/dev/ttyACM0"
     else:
+        print("Screen @/dev/ttyACM1")
         return "/dev/ttyACM1"
-
